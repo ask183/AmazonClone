60b822eaeccfc09399c42470fc29f4a59e2b3df5
ask183 <aabharan.koppal@gmail.com>
2022-09-06 00:48:07 -0400

    amazon clone ts and react native

---
+++ src/screens/HomeScreen/index.tsx
@@ -0,0 +1,25 @@
+import {View, StyleSheet, FlatList} from 'react-native';
+import React from 'react';
+import ProductItem from '../../components/ProductItem';
+import products from '../../data/products';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.page}>
+      {/* Render Product Component */}
+      <FlatList
+        data={products}
+        renderItem={({item}) => <ProductItem item={item} />}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 10,
+  },
+});
+
+export default HomeScreen;
